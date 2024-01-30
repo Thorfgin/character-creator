4a5a6e5bb6a92c8b307c195f63bb1e094494ea48
Jos van Rest <jvrest@nekst-it.com>
2024-01-30 15:09:27 +0100

    SVG back to assets

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './ui-assets/react.svg'
-import viteLogo from './ui-assets/vite.svg'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
